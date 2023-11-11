47cdbd8e6dbe7169c77788e1eaa0c0024a52515f
prachi <navaleprachi4@gmail.com>
2023-11-11 14:50:51 -0500

    Code added for Driver and Main classes

---
+++ src/Driver.java
@@ -0,0 +1,139 @@
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class Driver {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void driverOptions() throws SQLException {
+        boolean exit = false;
+        while (!exit) {
+            System.out.println("1. Insert ward information");
+            System.out.println("2. View all ward information");
+            System.out.println("3. Update ward information");
+            System.out.println("4. Delete ward information");
+            System.out.println("5. Return to the home screen");
+            System.out.println("Select suitable option");
+            System.out.println("Choose what operation you want to perform");
+
+            int choice;
+            while (true) {
+                try {
+                    System.out.print("Enter your choice: ");
+                    choice = Integer.parseInt(scanner.nextLine());
+                    break;
+                } catch (Exception e) {
+                    System.out.println("Please enter a valid choice (numerical)");
+                }
+            }
+
+            switch (choice) {
+                case 1 -> insertDriver();
+                case 2 -> viewDriver();
+                case 3 -> updateDriver();
+                case 4 -> deleteDriver();
+                case 5 -> System.out.println("Back to home menu");
+                default -> System.out.println("Invalid choice. Please try again.");
+            }
+        }
+    }
+
+    private static void insertDriver() {
+        System.out.println("Enter Driver Name: ");
+        String driverName = scanner.nextLine();
+        System.out.println("Enter Driver ID: ");
+        int driverId = Integer.parseInt(scanner.nextLine());
+        System.out.println("Enter Driver Status: ");
+        String driverStatus = scanner.nextLine();
+    }
+
+    private static void viewDriver() throws SQLException {
+        String query = "Select * from Driver";
+        PreparedStatement myStmt = Main.connection.prepareStatement(query);
+        ResultSet rs = myStmt.executeQuery();
+        DBTablePrinter.printResultSet(rs);
+    }
+
+    private static void deleteDriver() throws SQLException {
+        Scanner scanner = new Scanner(System.in);
+
+        boolean exit = false;
+        while (!exit) {
+            System.out.println("1. Delete driver information by driver ID");
+            System.out.println("2. Delete driver information by driver name");
+            System.out.println("3. Return to the driver screen");
+
+            int choice;
+            while (true) {
+                try {
+                    System.out.print("Enter your choice: ");
+                    choice = Integer.parseInt(scanner.nextLine());
+                    break;
+                } catch (Exception e) {
+                    System.out.println("Please enter a valid choice (numerical)");
+                }
+            }
+
+            switch (choice) {
+                case 1 -> deleteDriverById();
+                case 2 -> deleteDriverByName();
+                case 3 -> {
+                    System.out.println("Back to driver menu");
+                    exit = true;
+                }
+                default -> System.out.println("Invalid choice. Please try again.");
+            }
+        }
+    }
+
+    private static void deleteDriverById() throws SQLException {
+        scanner = new Scanner(System.in);
+        System.out.println("Enter Driver Id To Be Deleted: ");
+        int driverId = scanner.nextInt();
+
+        boolean driverExists = false;
+        ResultSet rs = Main.statement.executeQuery("SELECT * FROM Driver WHERE DriverID = \'" + driverId + "\'");
+        if (rs.next()) {
+            driverExists = true;
+        }
+
+        if (driverExists) {
+            Main.statement.executeUpdate("DELETE FROM Driver WHERE DriverID = \'" + driverId + "\';");
+            System.out.println("Driver row with ID " + driverId + " deleted successfully.");
+        } else {
+            System.out.println("Incorrect driver ID entered. Please select from the below driver IDs");
+            ResultSet ids = Main.statement.executeQuery("SELECT DriverID FROM Driver;");
+            while (ids.next()) {
+                System.out.println(ids.getInt("DriverID"));
+            }
+            System.out.println();
+        }
+    }
+
+    private static void deleteDriverByName() throws SQLException {
+        System.out.println("Enter Driver Name To Be Deleted: ");
+        int driverName = scanner.nextInt();
+
+        boolean driverExists = false;
+        ResultSet rs = Main.statement.executeQuery("SELECT * FROM Driver WHERE DriverName = \'" + driverName + "\'");
+        if (rs.next()) {
+            driverExists = true;
+        }
+
+        if (driverExists) {
+            Main.statement.executeUpdate("DELETE FROM Driver WHERE DriverName = \'" + driverName + "\';");
+            System.out.println("Driver row with Name " + driverName + " deleted successfully.");
+        } else {
+            System.out.println("Incorrect driver Name entered. Please select from the below drivers");
+            ResultSet name = Main.statement.executeQuery("SELECT DriverID FROM Driver;");
+            while (name.next()) {
+                System.out.println(name.getInt("DriverName"));
+            }
+            System.out.println();
+        }
+    }
+
+    private static void updateDriver() {
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,121 @@
+//package src;
+import java.sql.*;
+import java.util.Scanner;
+
+public class Main {
+    static final String USERNAME = "pnavale";
+    static final String PW = "*****";
+    static final String jdbcURL = "jdbc:mariadb://classdb2.csc.ncsu.edu:3306/pnavale";
+    // Put your oracle ID and password here
+
+    public static Connection connection = null;
+    public static Statement statement = null;
+
+    public static final ResultSet result = null;
+//    public static final ResultSet result = null;
+
+    /**
+     * This function prints a menu to the system console.
+     * Through this menu, one can interact with this program.
+     * The first step must always be to load the database ("Initialize/Reload Database"),
+     * if it is not already loaded. Then one can either look at reports,
+     * handle payments, or otherwise interact with the database.
+     * @param args
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        try {
+            connectToDatabase();
+
+            // Display the main menu
+            boolean exit = false;
+            while (!exit) {
+                System.out.println("Menu:");
+                System.out.println("1. Driver");
+                System.out.println("2. Parking Lot");
+                System.out.println("3. Permit");
+                System.out.println("4. Vehicle");
+                System.out.println("5. Citation");
+                System.out.println("6. Reports");
+                System.out.println("7. Initialize/Reload Database");
+                System.out.println("8. Exit()");
+
+                int choice;
+                while(true) {
+                    try {
+                        System.out.print("Enter your choice: ");
+                        choice = Integer.parseInt(scanner.nextLine());
+                        break;
+                    }
+                    catch(Exception e) {
+                        System.out.println("Please enter a valid choice (numerical)");
+                    }
+                }
+
+                // Perform the selected action
+                switch (choice) {
+                    case 1 -> Driver.driverOptions();
+//                    case 2 -> deleteMenu();
+//                    case 3 -> updateMenu();
+//                    case 4 -> payment();
+//                    case 5 -> reportsMenu();
+//                    case 6 -> initialize();
+                    case 8 -> exit = true;
+                    default -> System.out.println("Invalid choice. Please try again.");
+                }
+            }
+        } catch (SQLException | ClassNotFoundException e) {
+            System.out.println(e.getMessage());
+            throw new RuntimeException(e);
+        }
+        finally {
+            System.out.println("\nCLOSING the Database connection finally!");
+            close();
+        }
+    }
+
+    /**
+     * Connects to the database.
+     * @throws ClassNotFoundException
+     * @throws SQLException
+     */
+    public static void connectToDatabase() throws ClassNotFoundException, SQLException {
+        try {
+            Class.forName("org.mariadb.jdbc.Driver");
+            connection = DriverManager.getConnection(jdbcURL, USERNAME, PW);
+            statement = connection.createStatement();
+        } catch (SQLException e) {
+            throw new SQLException("Could not connect to database.");
+        }
+        catch (Exception e) {
+            throw new ClassNotFoundException("Class for driver not found.");
+        }
+    }
+
+    /**
+     * Closes the connection to the database.
+     */
+    private static void close() {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        if (statement != null) {
+            try {
+                statement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        if (result != null) {
+            try {
+                result.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
